8fc04c6d16663c046b606a584e6f3908a35f4297
Agil Dhason <adhason@umich.edu>
2022-04-21 22:16:20 -0400

    Added tests

--- tests.sh
+++ tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo "---"
-./bible Philemon 1:4-6 --version NRSV --verse-number
+./bible Philemon 1:4-6 --version NRSV 
 echo "---"
-./bible Philemon 1:4-6 --version NKJV --verse-number
+./bible Philemon 1:4-6 --version NKJV 
 echo "---"
+./bible 1 Peter 5:7 --version NKJV
+echo "---"
+./bible 1 Peter 5:7 --version NRSV
+echo "---"
+./bible 2 Corinthians 5:17 --version NKJV
+echo "---"
+./bible 2 Corinthians 5:17 --version NRSV
