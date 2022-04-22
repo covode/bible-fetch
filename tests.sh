fa2646155d3559ecd89e98c34065b582654f6c63
Agil Dhason <adhason@umich.edu>
2022-04-21 21:56:52 -0400

    Other files used

---
+++ tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "---"
+./bible Philemon 1:4-6 --version NRSV --verse-number
+echo "---"
+./bible Philemon 1:4-6 --version NKJV --verse-number
+echo "---"
---
+++ verse-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "---"
+./bible Psalm 23 --version NKJV --verse-number
+echo "---"
